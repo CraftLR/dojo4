7973127fe020a156926a181442747435e77f4ac5
jrx <jerome+git@rouaix.eu>
2023-05-03 20:45:49 +0200

    you can have tests in the same file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+}
+
+// tests module
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn usually_rust_tests_are_just_in_the_same_file() {
+    assert!(matches!(Some(true), Some(x) if x));
+  }
 }
